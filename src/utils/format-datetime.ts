b91272e1a7d347216c463e8e76d92ee846c7f729
jose <josejr017@gmail.com>
2025-05-23 11:28:58 -0300

    functions to format dates

---
+++ src/utils/format-datetime.ts
@@ -0,0 +1,14 @@
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+export function formatDateTime(rawDate: string): string {
+  const date = new Date(rawDate);
+
+  return format(date, "dd/MM/yyyy 'às' HH'h'mm", { locale: ptBR });
+}
+
+export function formatToRelativeDate(rawDate: string): string {
+  const date = new Date(rawDate);
+
+  return formatDistanceToNow(date, { locale: ptBR, addSuffix: true });
+}
