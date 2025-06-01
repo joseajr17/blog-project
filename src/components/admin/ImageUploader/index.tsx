3d5259dc2364bd9d5e04a53cac8e4c5e6590b1a9
jose <josejr017@gmail.com>
2025-06-01 15:41:04 -0300

    showing a image preview

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -3,12 +3,13 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { IMAGE_UPLOADER_MAX_SIZE } from "@/lib/constants";
 import { ImageUpIcon } from "lucide-react";
-import { useRef, useTransition } from "react";
+import { useRef, useState, useTransition } from "react";
 import { toast } from "react-toastify";
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [isPending, startTransition] = useTransition();
+  const [imgUrl, setImgUrl] = useState('');
 
   function handleClick() {
     const fileInput = fileInputRef.current;
@@ -24,16 +25,23 @@ export function ImageUploader() {
 
     const fileInput = fileInputRef.current;
 
-    if (!fileInput) return;
+    if (!fileInput) {
+      setImgUrl('');
+      return;
+    }
 
     const file = fileInput?.files?.[0];
 
-    if (!file) return;
+    if (!file) {
+      setImgUrl('');
+      return;
+    }
 
     if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
       const readableMaxSize = IMAGE_UPLOADER_MAX_SIZE / 1024;
       toast.error(`Imagem muito grande. Máx.: ${readableMaxSize}KB.`);
 
+      setImgUrl('');
       fileInput.value = '';
       return;
     }
@@ -47,22 +55,35 @@ export function ImageUploader() {
       if (result.error) {
         toast.error(result.error);
         fileInput.value = '';
+        setImgUrl('');
         return;
       }
 
-      toast.success(result.url);
-
+      setImgUrl(result.url);
+      toast.success('Imagem enviada com sucesso!');
     });
-
     fileInput.value = '';
   }
 
   return (
-    <div className="flex py-6">
-      <Button onClick={handleClick} variant='outline' type="button"
-        className="border border-slate-400 dark:border-slate-400 cursor-pointer">
-        <ImageUpIcon /> Enviar uma imagem</Button>
-      <Input onChange={handleChange} ref={fileInputRef} name="file" type="file" accept="image/*" hidden />
+    <div className="flex flex-col py-6 gap-4">
+      <Button onClick={handleClick} type="button" variant='outline' disabled={isPending}
+        className="self-start border border-slate-400 dark:border-slate-400 cursor-pointer">
+        <ImageUpIcon /> Enviar uma imagem
+      </Button>
+
+      {!!imgUrl && (
+        <div className='flex flex-col gap-4'>
+          <p>
+            <b>URL:</b> {imgUrl}
+          </p>
+
+          {/* eslint-disable-next-line */}
+          <img className='rounded-lg' src={imgUrl} />
+        </div>
+      )}
+
+      <Input onChange={handleChange} ref={fileInputRef} name="file" type="file" accept="image/*" disabled={isPending} hidden />
     </div>
   );
 }
