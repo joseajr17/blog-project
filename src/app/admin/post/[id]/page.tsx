fa2ecf7d7a6867fd9b9c23237b1deb7197fbb2a8
jose <josejr017@gmail.com>
2025-07-02 11:45:43 -0300

    AdminPostIdPage

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,15 +1,34 @@
+import { PostForm } from "@/components/admin/PostForm";
+import { makePublicPostFromDb } from "@/dto/post/dto";
+import { postRepository } from "@/repositories/post";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+
 export const dynamic = 'force-dynamic';
+
+export const metaData: Metadata = {
+  title: 'Editar post',
+};
 
 type AdminPostIdPageProps = {
   params: Promise<{
     id: string;
   }>
-}
+};
 
 export default async function AdminPostIdPage({ params }: AdminPostIdPageProps) {
   const { id } = await params;
+  const post = await postRepository.findById(id).catch();
+
+  if (!post)
+    notFound();
+
+  const publicPost = makePublicPostFromDb(post);
 
   return (
-    <div className="py-16 text-5xl">Admin Post Page {id} </div>
+    <div className='flex flex-col gap-6'>
+      <h1 className='text-xl font-extrabold'>Editar post</h1>
+      <PostForm publicPost={publicPost} />
+    </div>
   );
 }
