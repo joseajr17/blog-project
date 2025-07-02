b3696e5b78b98d26356cf37ad4229d7204d53cab
jose <josejr017@gmail.com>
2025-07-02 11:38:07 -0300

    AdminPostNewPage metaData

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,11 +1,17 @@
 import { PostForm } from "@/components/admin/PostForm";
+import { Metadata } from "next";
 
 export const dynamic = 'force-dynamic';
 
+export const metaData: Metadata = {
+  title: 'Criar post',
+};
+
 export default async function AdminPostNewPage() {
   return (
-    <>
+    <div>
+      <h1 className='text-xl font-extrabold'>Criar post</h1>
       <PostForm />
-    </>
+    </div>
   );
 }
