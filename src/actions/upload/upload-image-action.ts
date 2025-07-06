bbbdf53940db859602a45034853d7354f410022d
jose <josejr017@gmail.com>
2025-07-06 20:16:03 -0300

    adjusting code to use environment variables

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,6 +1,5 @@
 'use server';
 
-import { IMAGE_SERVER_URL, IMAGE_UPLOAD_DIRECTORY, IMAGE_UPLOADER_MAX_SIZE } from "@/lib/constants";
 import { mkdir, writeFile } from "fs/promises";
 import { extname, resolve } from "path";
 
@@ -21,7 +20,8 @@ export async function uploadImageAction(formData: FormData): Promise<UploadImage
   if (!(file instanceof File))
     return makeResult({ error: 'Arquivo inválido' });
 
-  if (file.size > IMAGE_UPLOADER_MAX_SIZE)
+  const uploadMaxSize = Number(process.env.NEXT_PUBLIC_IMAGE_UPLOAD_MAX_SIZE) || 921600;
+  if (file.size > uploadMaxSize)
     return makeResult({ error: 'Arquivo muito grande' });
 
   if (!file.type.startsWith('image/'))
@@ -31,7 +31,8 @@ export async function uploadImageAction(formData: FormData): Promise<UploadImage
   const imageExtesion = extname(file.name);
   const uniqueImageName = `${Date.now()}${imageExtesion}`;
 
-  const uploadFullPath = resolve(process.cwd(), 'public', IMAGE_UPLOAD_DIRECTORY);
+  const uploadDir = process.env.IMAGE_UPLOAD_DIRECTORY || 'uploads';
+  const uploadFullPath = resolve(process.cwd(), 'public', uploadDir);
 
   await mkdir(uploadFullPath, { recursive: true });
 
@@ -42,9 +43,8 @@ export async function uploadImageAction(formData: FormData): Promise<UploadImage
 
   await writeFile(fileFullPath, buffer);
 
-  const url = `${IMAGE_SERVER_URL}/${uniqueImageName}`;
-
+  const imgServerUrl = process.env.IMAGE_SERVER_URL || 'http://localhost:3000/uploads';
+  const url = `${imgServerUrl}/${uniqueImageName}`;
 
   return makeResult({ url });
-
 }
--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,7 +1,6 @@
 import { uploadImageAction } from "@/actions/upload/upload-image-action";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { IMAGE_UPLOADER_MAX_SIZE } from "@/lib/constants";
 import { ImageUpIcon } from "lucide-react";
 import { useRef, useState, useTransition } from "react";
 import { toast } from "react-toastify";
@@ -41,8 +40,10 @@ export function ImageUploader({ disabled = false }: ImageUploaderProps) {
       return;
     }
 
-    if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
-      const readableMaxSize = IMAGE_UPLOADER_MAX_SIZE / 1024;
+    const uploadMaxSize = Number(process.env.NEXT_PUBLIC_IMAGE_UPLOAD_MAX_SIZE) || 921600;
+
+    if (file.size > uploadMaxSize) {
+      const readableMaxSize = (uploadMaxSize / 1024).toFixed(2);
       toast.error(`Imagem muito grande. Máx.: ${readableMaxSize}KB.`);
 
       setImgUrl('');
